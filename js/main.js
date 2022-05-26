7559803bafc7c1f68a5479f83ca804a97d270eff
Григорий <89094284+gjazzzy@users.noreply.github.com>
2022-05-26 22:33:40 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-  $(document).ready(function () {
+$(document).ready(function () {
     $(".header__menu-btn").on('click', function (e) {
       e.praventDefault;
       $(this).toggleClass('header__menu-btn_active');
     });
 
   });
+
+    $(function () {
+    $('.header__hamburger-btn').on('click',function (){
+        $('.header__modal').removeClass('header__modal--close');
+    })
+    $('.modal_menu-close').on('click',function (){
+        $('.header__modal').addClass('header__modal--close');
+    })
+})
